d467d30d56a373d377641e803c03ceb2d679ba9a
Ning Liu <nliu@microsoft.com>
2025-06-25 16:14:11 +0800

    refactor: update message extension sso sample dependency

--- query-org-user-with-message-extension-sso/index.ts
+++ query-org-user-with-message-extension-sso/index.ts
@@ -5,26 +5,25 @@ import send from "send";
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 import {
-  CloudAdapter,
   ConfigurationServiceClientCredentialFactory,
-  ConfigurationBotFrameworkAuthentication,
   TurnContext,
+  MemoryStorage,
+  MessagingExtensionAttachment,
+  CardFactory,
 } from "botbuilder";
 
-// This bot's main dialog.
-import { TeamsBot } from "./teamsBot";
 import config from "./config";
+import { ApplicationBuilder,  TeamsAdapter, TurnState } from "@microsoft/teams-ai";
+import { Client, ResponseType } from "@microsoft/microsoft-graph-client";
 
 // Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about adapters.
-const credentialsFactory = new ConfigurationServiceClientCredentialFactory(config);
-
-const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
-  {},
-  credentialsFactory
+const adapter = new TeamsAdapter(
+    {},
+    new ConfigurationServiceClientCredentialFactory({
+        MicrosoftAppId: config.MicrosoftAppId,
+        MicrosoftAppPassword: config.MicrosoftAppPassword,
+    })
 );
-
-const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
 const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
@@ -53,9 +52,6 @@ const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
 // Set the onTurnError for the singleton BotFrameworkAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
-// Create the bot that will handle incoming messages.
-const bot = new TeamsBot();
-
 // Create express application.
 const expressApp = express();
 expressApp.use(express.json());
@@ -70,17 +66,104 @@ const server = expressApp.listen(
   }
 );
 
+const storage = new MemoryStorage();
+const app = new ApplicationBuilder()
+    .withStorage(storage)
+    .withAuthentication(adapter, {
+        settings: {
+            graph: {
+                scopes: ["User.Read.All", "User.Read"],
+                msalConfig: {
+                    auth: {
+                        clientId: config.clientId,
+                        clientSecret: config.clientSecret,
+                        authority: `${config.authorityHost}/${config.tenantId}`
+                    }
+                },
+                signInLink: `https://${config.botDomain}/auth-start.html`,
+                endOnInvalidMessage: true
+            }
+        },
+        autoSignIn: true
+    })
+    .build();
+
+app.messageExtensions.query('searchQuery', async (_context: TurnContext, state: TurnState, query) => {
+  const ssoToken = state.temp.authTokens['graph'];
+  if (!ssoToken) {
+    throw new Error('No auth token found in state. Authentication failed.');
+  }
+
+  const searchContext = query.parameters.searchQuery;
+  const results: MessagingExtensionAttachment[] = [];
+
+  const graphClient = Client.init({
+    authProvider: (done) => {
+      done(null, ssoToken);
+    }
+  });
+  let users = await graphClient
+    .api(`/users?$search="displayName:${searchContext}"&$count=true`)
+    .header("ConsistencyLevel", "eventual")
+    .orderby("displayName")
+    .get();
+  for (const user of users.value) {
+    await getUserPhotoWithGraphClient(
+      graphClient,
+      results,
+      user,
+      `/users/${user.id}/photo/$value`
+    );
+  };
+
+  return {
+    type: "result",
+    attachmentLayout: "list",
+    attachments: results,
+  };
+});
+
+app.messageExtensions.selectItem(async (_context: TurnContext, _state: TurnState, item) => {
+    // Return results
+    return {
+        attachmentLayout: 'list',
+        attachments: [CardFactory.heroCard(item.name, item.description)],
+        type: 'result'
+    };
+});
+
+app.messageExtensions.queryLink(async (_context: TurnContext, state: TurnState, _url: string) => {
+    const ssoToken = state.temp.authTokens['graph'];
+    if (!ssoToken) {
+        throw new Error('No auth token found in state. Authentication failed.');
+    }
+  const results: MessagingExtensionAttachment[] = [];
+
+  const graphClient = Client.init({
+    authProvider: (done) => {
+      done(null, ssoToken);
+    }
+  });
+  const profile = await graphClient.api("/me").get();
+  await getUserPhotoWithGraphClient(
+    graphClient,
+    results,
+    profile,
+    `/me/photo/$value`
+  );
+
+    return {
+        type: 'result',
+        attachments: results,
+        attachmentLayout: 'list'
+    };
+});
+
 // Listen for incoming requests.
 expressApp.post("/api/messages", async (req, res) => {
   await adapter
     .process(req, res, async (context) => {
-      await bot.run(context);
-    })
-    .catch((err) => {
-      // Error message including "412" means it is waiting for user's consent, which is a normal process of SSO, sholdn't throw this error.
-      if (!err.message.includes("412")) {
-        throw err;
-      }
+      await app.run(context);
     });
 });
 
@@ -94,3 +177,29 @@ expressApp.get(["/auth-start.html", "/auth-end.html"], async (req, res) => {
     )
   ).pipe(res);
 });
+
+async function getUserPhotoWithGraphClient(
+  graphClient,
+  attachments,
+  user,
+  apiPath
+) {
+  let image = undefined;
+  try {
+    let photoBinary = await graphClient
+      .api(apiPath)
+      .responseType(ResponseType.ARRAYBUFFER)
+      .get();
+    const buffer = Buffer.from(photoBinary);
+    const imageUri = "data:image/png;base64," + buffer.toString("base64");
+    image = CardFactory.images([imageUri]);
+  } catch (err) {
+    console.error("This user may not have personal photo!", err.message);
+  }
+  const thumbnailCard = CardFactory.thumbnailCard(
+    user.displayName,
+    user.mail,
+    image ? image : ""
+  );
+  attachments.push(thumbnailCard);
+}
