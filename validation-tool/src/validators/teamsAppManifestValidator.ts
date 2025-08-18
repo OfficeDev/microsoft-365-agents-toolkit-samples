9eade75a315f73820fb1334a9db39d870927e489
Ning Tang <nintan@microsoft.com>
2025-08-18 16:55:49 +0800

    fix: validation rule (#1480)
    * fix: validation rule
    
    * fix: sample tag
    
    * fix: comment
    
    * fix: update tool
    
    * fix: comments

--- validation-tool/src/validators/teamsAppManifestValidator.ts
+++ validation-tool/src/validators/teamsAppManifestValidator.ts
@@ -3,32 +3,35 @@
 
 import fs from "fs-extra";
 import path from "path";
+import semver from "semver";
 import { Result } from "../resultType";
 
-const MANIFEST_VERSION = "1.17";
+const LATEST_MANIFEST_VERSION = "1.22.0";
 const MANIFEST_PREVIEW_VERSION = "devPreview";
 
 /**
  * Rule 1: Manifest id is referencing placeholder from env: ${{TEAMS_APP_ID}}
  * Rule 2: Manifest version should be latest to align with TTK
- * 
+ *
  * @param projectDir root directory of the project
  * @returns validation result
  */
-export default async function validateTeamsAppManifest(projectDir: string): Promise<Result> {
+export default async function validateTeamsAppManifest(
+  projectDir: string
+): Promise<Result> {
   const result: Result = {
-    name: "Teams App Manifest",
+    name: "App Manifest",
     passed: [],
     failed: [],
     warning: [],
   };
 
   const manifestFile = path.join(projectDir, "appPackage", "manifest.json");
-  if (!await fs.exists(manifestFile)) {
+  if (!(await fs.exists(manifestFile))) {
     result.failed = [`appPackage/manifest.json does not exist.`];
     return result;
   }
-  const fileContent = await fs.readFile(manifestFile, 'utf8');
+  const fileContent = await fs.readFile(manifestFile, "utf8");
   let jsonData: Record<string, unknown> | undefined;
   try {
     jsonData = JSON.parse(fileContent);
@@ -41,14 +44,28 @@ export default async function validateTeamsAppManifest(projectDir: string): Prom
   if (!appId || appId !== "${{TEAMS_APP_ID}}") {
     result.failed.push(`id should be equal to '\${{TEAMS_APP_ID}}'.`);
   } else {
-    result.passed.push(`id is referencing placeholder from env: \${{TEAMS_APP_ID}}.`);
+    result.passed.push(
+      `id is referencing placeholder from env: \${{TEAMS_APP_ID}}.`
+    );
   }
-  if (jsonData.manifestVersion === MANIFEST_VERSION) {
-    result.passed.push(`Manifest version is aligned with Microsoft 365 Agents Toolkit.`);
-  } else if (jsonData.manifestVersion === MANIFEST_PREVIEW_VERSION) {
-    result.warning.push(`Manifest version(${MANIFEST_PREVIEW_VERSION}) is using preview version.`);
+  if (jsonData.manifestVersion === MANIFEST_PREVIEW_VERSION) {
+    result.warning.push(
+      `Manifest version(${MANIFEST_PREVIEW_VERSION}) is using preview version.`
+    );
   } else {
-    result.failed.push(`Manifest version(${jsonData.manifestVersion}) is NOT aligned with Microsoft 365 Agents Toolkit(${MANIFEST_VERSION}).`);
+    const manifestVersion = semver.coerce(jsonData.manifestVersion as string);
+    if (
+      manifestVersion &&
+      semver.eq(manifestVersion, LATEST_MANIFEST_VERSION)
+    ) {
+      result.passed.push(
+        `Manifest version is aligned with Microsoft 365 Agents Toolkit.`
+      );
+    } else {
+      result.warning.push(
+        `Manifest version(${jsonData.manifestVersion}) is NOT aligned with Microsoft 365 Agents Toolkit(${LATEST_MANIFEST_VERSION}).`
+      );
+    }
   }
   return result;
 }
