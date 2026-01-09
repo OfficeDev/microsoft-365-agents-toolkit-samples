41d6df44c659d57718cccd74f0d550b337e288fd
Kavin Singh <kavinsingh@microsoft.com>
2026-01-09 11:10:07 -0800

    filter by message activity

--- share-now/bot/index.ts
+++ share-now/bot/index.ts
@@ -44,10 +44,11 @@ const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
       'https://www.botframework.com/schemas/error',
       'TurnError'
     );
-  
-    // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+
+    if (context.activity.type == 'message') {  
+      // Send a message to the user
+      await context.sendActivity('The bot encountered an error or bug.');
+    }
 };
 
 // Set the onTurnError for the singleton BotFrameworkAdapter.
